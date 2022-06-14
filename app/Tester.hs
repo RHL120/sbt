9aeaa652f636fa8cb2d004b2877dcd4eac153f66
rhl120 <rhl120@protonmail.com>
2022-06-14 21:44:08 +0200

    Simplifiy the test structure and implement it's functions

--- app/Tester.hs
+++ app/Tester.hs
@@ -1,40 +1,59 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Tester where
 
 import Data.List (isInfixOf)
 import qualified Data.Map as Map
+import GHC.IO.Handle (hGetContents)
+import System.Process
+  ( StdStream(CreatePipe)
+  , createProcess
+  , shell
+  , std_out
+  , waitForProcess
+  )
 import Text.Printf (printf)
 
-type Command = String
+type Condition = (String -> Bool)
 
-type Condition = String -> Bool
-
-type ConditionConstructor
-   = [String] -> Either String (String -> Either Error String)
-
-type Error = String
+type ConditionConstructor = [String] -> Either String Condition
 
 data Test =
   Test
     { testName :: String
-    , testConditions :: [ConditionConstructor]
-    , testCommand :: String
+    , testCmd :: String
+    , testConditions :: [Condition]
     }
+
+data TestResult =
+  TestResult
+    { testedTest :: Test
+    , testedOutput :: String
+    , result :: Bool
+    }
+  deriving (Show)
 
 type Script = [Test]
 
-contains :: ConditionConstructor
-contains [] = Left "Expected one argument"
-contains (x:xs) =
-  Right $ \y ->
-    if x `isInfixOf` y
-      then Right $ printf "%s does contain %s" y x
-      else Left $ printf "%s does not contain %s" x y
+instance Show Test where
+  show (Test name cmd _) = printf "name=%s, cmd=%s" name cmd
 
-conditionsConstructors :: Map.Map String ConditionConstructor
-conditionsConstructors = Map.fromList [("contains", contains)]
+condCons :: Map.Map String ConditionConstructor
+condCons =
+  Map.fromList
+    [ ( "contain"
+      , \case
+          [] -> Left "Usage: contains(string)"
+          (x:_) -> Right (isInfixOf x))
+    , ("be_empty", const $ Right null)
+    ]
 
-runTest :: Test -> IO (Either Error String)
-runTest = error "TODO: implement runTest"
+runTest :: Test -> IO TestResult
+runTest t@(Test _ cmd conds) = do
+  (_, Just out, _, handle) <- createProcess (shell cmd) {std_out = CreatePipe}
+  waitForProcess handle
+  output <- hGetContents out
+  return (TestResult t "" (all (\f -> f output) conds))
 
-runScript :: Script -> IO [Either Error String]
-runScript = error "TODO: implement runScript"
+runScript :: Script -> IO [TestResult]
+runScript = traverse runTest
